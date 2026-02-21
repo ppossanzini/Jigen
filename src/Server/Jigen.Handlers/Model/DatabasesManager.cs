219ba4430e5ff69db8ae742cc6d74a826bc37e0b
Paolo Possanzini <p.possanzini@teamdev.it>
2026-02-21 11:45:36 +0100

    fixed masterdb init

--- src/Server/Jigen.Handlers/Model/DatabasesManager.cs
+++ src/Server/Jigen.Handlers/Model/DatabasesManager.cs
@@ -2,12 +2,32 @@ using Microsoft.Extensions.Options;
 
 namespace Jigen.Handlers.Model;
 
-public class DatabasesManager()
+public class DatabasesManager
 {
-  public Dictionary<string, Store> ActiveDatabases { get; init; } = new();
+  private readonly SystemDB _master;
+  private readonly JigenServerSettings _settings;
+    
+  public DatabasesManager(SystemDB master, IOptions<JigenServerSettings> settings)
+  {
+    this._master = master;
+    this._settings = settings.Value;
+    Init();
+  }
   
+  public Dictionary<string, Store> ActiveDatabases { get; init; } = new();
+
+  public void Init()
+  {
+    var dbs = _master.System[SystemDB.BASEINFO].Databases;
+    foreach (var db in dbs)
+    {
+      if(ActiveDatabases.ContainsKey(db))
+        continue;
+      ActiveDatabases.Add(db, new Store(new StoreOptions(){DataBaseName = db, DataBasePath = _settings.DataFolderPath}));
+    }
+  }
+
   public void FlushAndStopALL()
   {
-    
   }
 }
